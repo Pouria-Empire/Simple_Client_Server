5f7c98bd33c2f299000f17aa5dc2887e75d7dd28
Pouria ArefiJamal <74317596+Pouria-Empire@users.noreply.github.com>
2023-03-04 18:38:22 +0330

    Update sclient.py

--- sclient.py
+++ sclient.py
@@ -1,11 +1,15 @@
 import socket
-
+import random
+import string
+S = 10 
 HOST = '127.0.0.1'  # The server's hostname or IP address
 PORT = 65432        # The port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    s.sendall(b'Hello, world')
+    ran = "The Flag is : " + str(''.join(random.choices(string.ascii_uppercase + string.digits, k = S)))  
+    recv_data = ran.encode('ascii')
+    s.sendall(recv_data)
     data = s.recv(1024)
 
 print('Received', repr(data))
